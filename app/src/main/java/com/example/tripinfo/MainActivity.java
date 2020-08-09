15317a5b7e55c14b79edc92b237b2c63f78d5cc6
Vanessa Git <vanesssa.chia-hui-hsu@hotmail.com>
2020-08-09 03:11:00 +0200

    + date option

--- app/src/main/java/com/example/tripinfo/MainActivity.java
+++ app/src/main/java/com/example/tripinfo/MainActivity.java
@@ -1,70 +1,72 @@
 package com.example.tripinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    DatePickerDialog picker;
-    DatePickerDialog.OnDateSetListener mDateSetListener;
+    DatePickerDialog userDatePicker;
+    TimePickerDialog userTimePicker;
+    EditText showUserDate;
+    EditText showUserTime;
 
-    EditText eText;
-    Button btnGet;
-    TextView tvw;
-
-    DatePickerFragement datePickerFragment = new DatePickerFragement();
-
-    TimePickerFragment timePickerFragment = new TimePickerFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // tvw=(TextView)findViewById(R.id.textView1);
-        eText=(EditText) findViewById(R.id.editText1);
 
-
-        eText.setInputType(InputType.TYPE_NULL);
-        eText.setOnClickListener(new View.OnClickListener() {
+        showUserDate=(EditText) findViewById(R.id.enterDate);
+        showUserDate.setInputType(InputType.TYPE_NULL);
+        showUserDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final Calendar cldr = Calendar.getInstance();
                 int day = cldr.get(Calendar.DAY_OF_MONTH);
                 int month = cldr.get(Calendar.MONTH);
                 int year = cldr.get(Calendar.YEAR);
+
                 // date picker dialog
-                picker = new DatePickerDialog(MainActivity.this,
+                userDatePicker = new DatePickerDialog(MainActivity.this,
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year + "  ");
+                                showUserDate.setText(" " + dayOfMonth + " / " + (monthOfYear + 1) + " / " + year + "  ");
                             }
                         }, year, month, day);
-                picker.show();
+
+                        userDatePicker.show();
             }
         });
 
+        showUserTime=(EditText) findViewById(R.id.enterTime);
+        showUserTime.setInputType(InputType.TYPE_NULL);
+        showUserTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldt = Calendar.getInstance();
+                int hour = cldt.get(Calendar.HOUR_OF_DAY);
+                int minute = cldt.get(Calendar.MINUTE);
 
-    }
+                // time picker dialog
+                userTimePicker = new TimePickerDialog(MainActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,int minute) {
+                                showUserTime.setText(" "+hourOfDay + " : " + minute);
+                            }
+                        }, hour, minute, true);
 
-    public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "timePicker");
-    }
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragement();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
+                           userTimePicker.show();
+            }
+        });
     }
 }
 
